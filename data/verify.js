8e7198dd47f9ddfd63d83477d78325a8308a635f
aparna-sripal <55523417+aparna-sripal@users.noreply.github.com>
2021-12-07 14:50:26 -0500

    Update verify.js

--- data/verify.js
+++ data/verify.js
@@ -8,41 +8,11 @@ function validBoolean(bool) {
     return true;
 }
 
-function convertId(doc) {
-    doc._id = doc._id.toString();
-    return doc;
+function convertId(id) {
+    id._id = id._id.toString();
+    return id;
 }
 
-/*function validAge(age){
-    if (!age || typeof age != 'number' || !Number.isInteger(age) || age < 1) return false;
-    return true;
-}
-
-function validRating(num){
-    if (!num || (typeof num != 'number') || !Number.isInteger(num) || num < 1 || num > 5) return false;
-    return true;
-}*/
-
-function validEmail(email) {
-    if (!validString(email)) return false;
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-function validMetrics(metrics) {
-    if ((!metrics || typeof(metrics)!=='object') || (!validBoolean(metrics.distancedTables)) ||
-        (!validBoolean(metrics.maskedEmployees)) || (!validBoolean(metrics.noTouchPayment))  ||
-        (!validBoolean(metrics.outdoorSeating))  || (!validRating(metrics.price)) ||
-        (!validRating(metrics.rating))) return false;
-
-    return true;
-}
-
-function validLink(link) {
-    if (!validString(link)) return false;
-    const re = /^https:\/\/www\.yelp\.com\/biz\/((\w+)-)*\w+/;
-    return re.test(String(link).toLowerCase());
-}
 
 function generateList(n) {
     let arr = [];
@@ -55,11 +25,6 @@ function generateList(n) {
 module.exports = {
     validString,
     convertId,
-    //validAge,
-    //validEmail,
     validBoolean,
-    //validRating,
-    validMetrics,
-    validLink,
     generateList
 };
