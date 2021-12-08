379d9dae11a9c9167bd7b35a6de19b829a5ff8ef
ZiruiHE99 <95633441+ZiruiHE99@users.noreply.github.com>
2021-12-08 00:20:32 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -33,165 +33,26 @@ router.post("/search", async (req, res) => {
 });
 
 //LogIn Page POST Route
-// router.post("/login", async (req, res) => {
-//     let username = req.body.username;
-//     let password = req.body.password;
-
-//     if ((username) && (password)){
-
-//         if ((!username) || (!password)) {
-//             res.status(400).render('users/loginPage', {error: 'No Username/Password inputted!'});
-//             return;
-//         }
-//         if ((typeof username != 'string') || (!username.trim().length)) {
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-
-//         if ((typeof password != 'string') || (!password.trim().length)) {
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-
-//         if (username.indexOf(' ') >=0){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-//         if (password.indexOf(' ') >=0){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-
-//         if (username.length < 4){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-//         if (password.length < 6){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-
-//         var alphanum = /^[0-9a-zA-Z]+$/;
-//         if (!username.match(alphanum)){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password inputted incorrectly'});
-//             return;
-//         }
-//     }
-
-//     try {
-//         let auth = {};
-//         auth = await users.checkUser(username, password);
-
-//         if (auth.authenticated == true) {
-//             req.session.user = {username: username, password: password};
-//             sess = req.session.user;
-//             res.cookie("AuthCookie", req.session,false, true);
-//             res.redirect("/private");
-
-//         } else {
-//             res.status(400).render('users/loginPage', {error: e});
-//             return;
-//         }
-//     }
-//     catch (e) {
-//         if ((username == '') && (password == '')){
-//             res.status(400).render('users/loginPage', {error: 'Username/Password must be provided.'});
-//             return;
-//         }
-//         if ((!username) && (!password)){
-//             res.status(400).render('users/loginPage');
-//             return;
-
-//         } else {
-//             res.status(400).render('users/loginPage', {error: e});
-//             return;
-//         }
-//     }
-// });
-
-//SignUp Page Route
-router.get("/signup", async (req, res) => {
-    if (req.session.user){
-        res.redirect('/private');
-    } else{
-        res.render('users/signupPage');
-    }
-});
-
-
-//SignUp Page POST Route
-router.post("/signup", async (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-
-    // if ((username) && (password)){
-
-    //     if ((!username) || (!username)) {
-    //         res.status(400).render('users/signupPage', {error: 'No Username/Password inputted!'});
-    //         return;
-    //     }
-    //     if ((typeof username != 'string') || (!username.trim().length)) {
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-
-    //     if ((typeof password != 'string') || (!password.trim().length)) {
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-
-    //     if (username.indexOf(' ') >=0){
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-    //     if (password.indexOf(' ') >=0){
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-
-    //     if (username.length < 4){
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-    //     if (password.length < 6){
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-
-    //     var alphanum = /^[0-9a-zA-Z]+$/;
-    //     if (!username.match(alphanum)){
-    //         res.status(400).render('users/signupPage', {error: 'Username/Password inputted incorrectly'});
-    //         return;
-    //     }
-    // }
+router.post('/login', async (req, res) => {
     
-    try {
-        let create = await users.addUser(firstname, lastname);
-
-        if (!create){
-            res.status(500).render('users/signupPage', {error: 'Internal Server Error'});
-            return;
-
-        } else {
-            res.render('users/loginPage');
-            return;
+     const loginName = req.body.username;
+     const loginPW = req.body.password;
+try{
+    const logintest = await userdata.checkUser(loginName,loginPW)
+        if(logintest=='{authenticated: true}'){
+        req.session.user = { username:loginName };
+        res.redirect('/private');
         }
-    }
-    catch (e) {
-        if ((username == '') && (password == '')){
-            res.status(400).render('users/signupPage', {error: 'Username/Password must be provided.'});
-            return;
+        else{
+          res.render('users/loginpage', { title: "Login Page" });
         }
-        if ((!username) && (!password)){
-            res.status(400).render('users/signupPage', {error: 'Username/Password must be provided.'});
-            return;
-
-        } else {
-            res.status(400).render('users/signupPage', {error: e});
-            return;
-        }
-    }
+}
+catch (e) {
+    res.render('users/loginpage', { title: "Login Page" });
+  }
 });
+
+
 
 
 //get /private
