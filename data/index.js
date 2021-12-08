61cce0f30929c1f693eecb416159d530cd555b81
bnguyen03 <55515951+bnguyen03@users.noreply.github.com>
2021-12-07 19:26:19 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,9 +1,9 @@
 const postData = require("./posts");
 const userData = require("./users");
-const commentData = require("./comments');
+const commentData = require("./comments");
 
 module.exports = {
   users: userData,
   posts: postData,
-  comments: commentsData
+  comments: commentData,
 };
