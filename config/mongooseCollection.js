5ef4c8b550f6ae7ee131611434052cf45de850d0
bnguyen03 <55515951+bnguyen03@users.noreply.github.com>
2021-12-12 12:19:14 -0500

    Update mongooseCollection.js

--- config/mongooseCollection.js
+++ config/mongooseCollection.js
@@ -1,23 +1,23 @@
-const dbConnection = require('./mongooseConnection');
-const mongoose = require('mongoose');
-const Grid = require('gridfs-stream');
+const dbConnection = require("./mongooseConnections");
+const mongoose = require("mongoose");
+const Grid = require("gridfs-stream");
 
 const getCollectionFn = (collection) => {
-    let _col = undefined;
-    return async () => {
-        if (!_col) {
-            const db = await dbConnection();
-            _col = await Grid(db, mongoose.mongo);
-            _col.collection(collection);
-        }
-        return _col;
-    };
+  let _col = undefined;
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await Grid(db, mongoose.mongo);
+      _col.collection(collection);
+    }
+    return _col;
+  };
 };
 
 module.exports = {
-    uploads: getCollectionFn('uploads'),
-    // users: getCollectionFn('users'),
-    // reviews: getCollectionFn('reviews'),
-    // comments: getCollectionFn('comments'),
-    // posts: getCollectionFn('posts')
-}
+  uploads: getCollectionFn("uploads"),
+  // users: getCollectionFn('users'),
+  // reviews: getCollectionFn('reviews'),
+  // comments: getCollectionFn('comments'),
+  // posts: getCollectionFn('posts')
+};
