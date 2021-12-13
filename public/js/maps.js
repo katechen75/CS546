4f57c8928561bf7f2d648eb90664472749ac3151
bnguyen03 <55515951+bnguyen03@users.noreply.github.com>
2021-12-12 20:55:46 -0500

    Update maps.js

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,7 +1,8 @@
 // Derived from the Google Maps JS API documentation
 
-let script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC-T5kdfwDYmBUXQ6pw5xv4YkQc6bWsidE&callback=initMap';
+let script = document.createElement("script");
+script.src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEPhJJDBFNxN16BquzYkvcwG0ib-z5XoA&callback=initMap";
 script.defer = true;
 script.async = false;
 let currentLoc = zipcodes.lookup(posts.itemLocation);
@@ -9,12 +10,15 @@ let cityState = currentLoc.city + ", " + currentLoc.state;
 let latitude = currentLoc.latitude;
 let longitude = currentLoc.longitude;
 
-window.initMap = function() {
-    let gmap = new google.maps.Map(document.getElementById('gmap'), {
-        center: {lat: latitude, lng: longitude},
-        zoom: 16
-    });
-    let marker = new google.maps.Marker({position: {lat: latitude, lng: longitude}, map: gmap});
-}
+window.initMap = function () {
+  let gmap = new google.maps.Map(document.getElementById("gmap"), {
+    center: { lat: latitude, lng: longitude },
+    zoom: 16,
+  });
+  let marker = new google.maps.Marker({
+    position: { lat: latitude, lng: longitude },
+    map: gmap,
+  });
+};
 
 document.head.appendChild(script);
