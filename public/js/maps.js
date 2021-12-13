4fadc09ce8909d704192aa6c0313c70587609bdf
aparna-sripal <55523417+aparna-sripal@users.noreply.github.com>
2021-12-12 20:39:10 -0500

    Update maps.js

--- public/js/maps.js
+++ public/js/maps.js
@@ -4,9 +4,10 @@ let script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC-T5kdfwDYmBUXQ6pw5xv4YkQc6bWsidE&callback=initMap';
 script.defer = true;
 script.async = false;
-
-const latitude = parseFloat(document.currentScript.getAttribute('latitude'));
-const longitude = parseFloat(document.currentScript.getAttribute('longitude'));
+let currentLoc = zipcodes.lookup(posts.itemLocation);
+let cityState = currentLoc.city + ", " + currentLoc.state;
+let latitude = currentLoc.latitude;
+const longitude = currentLoc.longitude;
 
 window.initMap = function() {
     let gmap = new google.maps.Map(document.getElementById('gmap'), {
